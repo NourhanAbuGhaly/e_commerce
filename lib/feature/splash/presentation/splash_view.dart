05e636440123946c8a4f376d07173ecc594efd6f
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-19 01:15:32 +0300

    e_commerce 1

---
+++ lib/feature/splash/presentation/splash_view.dart
@@ -0,0 +1,14 @@
+import 'package:e_commerce/feature/splash/presentation/widgets/splash_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF69A03A),
+      body: SplashBody(),
+    );
+  }
+}
---
+++ lib/feature/splash/presentation/widgets/splash_body.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashBody extends StatelessWidget {
+  const SplashBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text.rich(
+            TextSpan(
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 51,
+                  color: Color(0xFFFFFFFF),
+                ),
+                children: [
+                  TextSpan(
+                      text: "F", style: TextStyle(fontWeight: FontWeight.w700)),
+                  TextSpan(
+                      text: "ruit Market",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 42))
+                ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
