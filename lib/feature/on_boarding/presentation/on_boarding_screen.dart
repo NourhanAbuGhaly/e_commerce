ccb77c28e28362c002c740d34d4ff831280032eb
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-23 01:35:43 +0300

    e_commerce 1

---
+++ lib/feature/on_boarding/presentation/on_boarding_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class onBoardingView extends StatelessWidget {
+  const onBoardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
--- lib/feature/splash/presentation/widgets/splash_body.dart
+++ lib/feature/splash/presentation/widgets/splash_body.dart
@@ -1,20 +1,80 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class SplashBody extends StatelessWidget {
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
 
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+  Animation<double>? fadingAnimation1;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+    fadingAnimation1 =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+    // ..addListener(() {
+    //   setState(() {
+    //     if (animationController!.isCompleted) {
+    //       animationController!.repeat(reverse: true);
+    //     }
+    //   });
+    // });
+    // animationController?.forward();
+    animationController!.repeat(reverse: true);
+  }
+@override
+  void dispose() {
+animationController!.dispose();
+super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        //  mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text.rich(
-            TextSpan(
+          Spacer(),
+          // AnimatedBuilder(
+          //   animation: fadingAnimation!,
+          //   builder: (context, _) => Opacity(
+          //     opacity: fadingAnimation!.value,
+          //     child: Text.rich(TextSpan(
+          //       style: TextStyle(
+          //         fontFamily: 'Poppins',
+          //         fontSize: 51,
+          //         color: Color(0xFFFFFFFF),
+          //       ),
+          //       children: [
+          //         TextSpan(
+          //             text: "F", style: TextStyle(fontWeight: FontWeight.w700)),
+          //         TextSpan(
+          //             text: "ruit Market",
+          //             style:
+          //                 TextStyle(fontWeight: FontWeight.w700, fontSize: 42))
+          //       ],
+          //     )),
+          //   ),
+          // ),
+          FadeTransition(
+            opacity: fadingAnimation1!,
+            child: Opacity(
+              opacity: fadingAnimation!.value,
+              child: Text.rich(TextSpan(
                 style: TextStyle(
                   fontFamily: 'Poppins',
                   fontSize: 51,
-                  fontWeight: FontWeight.bold,
                   color: Color(0xFFFFFFFF),
                 ),
                 children: [
@@ -24,8 +84,12 @@ class SplashBody extends StatelessWidget {
                       text: "ruit Market",
                       style:
                           TextStyle(fontWeight: FontWeight.w700, fontSize: 42))
-                ]),
+                ],
+              )),
+            ),
           ),
+          Spacer(),
+          Image.asset("assets/images/splash_view_image.png")
         ],
       ),
     );
