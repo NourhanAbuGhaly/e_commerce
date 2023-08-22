c29780e481e3b6045a7139a268cc4554cc2294ba
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-23 02:41:51 +0300

    e_commerce 1

--- lib/core/widget/custom_button.dart
+++ lib/core/widget/custom_button.dart
@@ -3,7 +3,8 @@ import 'package:e_commerce/core/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
 class CustomGeneralButton extends StatelessWidget {
-  const CustomGeneralButton({super.key});
+  const CustomGeneralButton({super.key, this.text});
+  final String? text;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class CustomGeneralButton extends StatelessWidget {
           color: KMaibColor, borderRadius: BorderRadius.circular(8)),
       child: Center(
         child: Text(
-          "Next",
+          text!,
           style: TextStyle(
               color: Color(0xffffffff),
               fontWeight: FontWeight.w500,
--- lib/feature/on_boarding/presentation/widgets/on_boarding_body.dart
+++ lib/feature/on_boarding/presentation/widgets/on_boarding_body.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/core/utils/size_config.dart';
+import 'package:e_commerce/core/widget/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class onBoardingViewBody extends StatelessWidget {
-  const onBoardingViewBody({super.key});
+  const onBoardingViewBody({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +18,21 @@ class onBoardingViewBody extends StatelessWidget {
           child: Text(
             "SKip",
             style: TextStyle(
-                fontFamily: "Poppins", fontSize: 14, color: Color(0xff898989)),
+              fontFamily: "Poppins",
+              fontSize: 14,
+              color: Color(0xff898989),
+            ),
             textAlign: TextAlign.left,
           ),
         ),
+        Positioned(
+          bottom: SizeConfig.defaultSize! * 10,
+          right: SizeConfig.defaultSize! * 10,
+          left: SizeConfig.defaultSize! * 10,
+          child: CustomGeneralButton(
+            text: "Next",
+          ),
+        )
       ],
     );
   }
