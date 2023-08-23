1edc94f4492b8155b89cf81235ff5c967b479732
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-23 09:43:05 +0300

    e_commerce 2

--- lib/core/widget/custom_button.dart
+++ lib/core/widget/custom_button.dart
@@ -1,13 +1,14 @@
 import 'package:e_commerce/core/constant.dart';
 import 'package:e_commerce/core/utils/size_config.dart';
+import 'package:e_commerce/core/widget/space_widget.dart';
 import 'package:flutter/material.dart';
 
 class CustomGeneralButton extends StatelessWidget {
-  const CustomGeneralButton(
-      {super.key, this.text, this.onTap});
+  const CustomGeneralButton({super.key, this.text, this.onTap});
 
   final String? text;
   final VoidCallback? onTap;
+
   // final IconData? iconData;
   // final Color? color;
 
@@ -29,6 +30,55 @@ class CustomGeneralButton extends StatelessWidget {
                 fontSize: 14),
             textAlign: TextAlign.left,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButtonWithIcon extends StatelessWidget {
+  const CustomButtonWithIcon(
+      {super.key, this.text, this.onTap, this.iconData, this.color});
+
+  final String? text;
+  final VoidCallback? onTap;
+  final IconData? iconData;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: SizeConfig.screenWidget,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Color(0xff707070),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              color: color,
+            ),
+            HorizientalSpace(
+              value: 2,
+            ),
+            Text(
+              text!,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontFamily: "Poppins",
+                fontSize: 12,
+                color: Color(0xff000000),
+              ),textAlign: TextAlign.left,
+            ),
+          ],
         ),
       ),
     );
--- lib/feature/Auth/presentation/pages/login/login_view_body.dart
+++ lib/feature/Auth/presentation/pages/login/login_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:e_commerce/core/constant.dart';
 import 'package:e_commerce/core/utils/size_config.dart';
+import 'package:e_commerce/core/widget/custom_button.dart';
 import 'package:e_commerce/core/widget/space_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -38,9 +40,24 @@ class LoginViewBody extends StatelessWidget {
         Row(
           children: [
             Flexible(
-                child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-            ))
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: CustomButtonWithIcon(
+                  color: Color(0xffdb3236),
+                  text: "Log in with",
+                  iconData: FontAwesomeIcons.googlePlusG,
+                ),
+              ),
+            ),  Flexible(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: CustomButtonWithIcon(
+                  color: Color(0xff4267B2),
+                  text: "Log in with",
+                  iconData: FontAwesomeIcons.facebookF,
+                ),
+              ),
+            ),
           ],
         )
       ],
